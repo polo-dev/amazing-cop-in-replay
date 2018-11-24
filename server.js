cdc63764a0be8475df2b53a1f487d132f34d4992
polo-dev <polo.reinhardt@gmail.com>
2018-11-24 10:57:55 +0100

    correctif of project

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var app = express();
  .use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000, https://api.spotify.com/*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 const apiSpotify = require('./server/spotify.js');
 
